1e7e41f6f183a89f225a8e2b4c36da056b7d7b1c
Quentin Brunet <hello@quentinbrunet.com>
2018-10-18 00:25:14 +0200

    Add custom fonts + colors

--- app/src/main/java/twodo/twodo/activity/MainActivity.java
+++ app/src/main/java/twodo/twodo/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package twodo.twodo.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 import twodo.twodo.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ViewPump.init(ViewPump.builder()
+            .addInterceptor(new CalligraphyInterceptor(
+                new CalligraphyConfig.Builder()
+                    .setDefaultFontPath("fonts/roboto_light.ttf")
+                    .setFontAttrId(R.attr.fontPath)
+                    .build()))
+            .build());
+
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 }
