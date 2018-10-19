3610ae32319cc66b34707959259d3c9e2ee215c4
Quentin Brunet <hello@quentinbrunet.com>
2018-10-19 03:45:29 +0200

    Hydrate my profile fragment - 2

--- app/src/main/java/twodo/twodo/activity/ProfileFragment.java
+++ app/src/main/java/twodo/twodo/activity/ProfileFragment.java
@@ -6,15 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import twodo.twodo.R;
+import twodo.twodo.dto.api.ApiListener;
+import twodo.twodo.dto.api.ApiProvider;
+import twodo.twodo.dto.model.Category;
 import twodo.twodo.dto.model.User;
 
 public class ProfileFragment extends Fragment {
@@ -28,7 +36,6 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.spinner2_profile) Spinner spinner2;
     @BindView(R.id.offer_description_profile) TextView description;
     @BindView(R.id.btn_add_category_profile) Button addCategory;
-    @BindView(R.id.btn_save_profile) Button saveProfile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +53,7 @@ public class ProfileFragment extends Fragment {
         phone.setText(User.getCurrentUser().getPhone());
         address.setText(User.getCurrentUser().getAddress());
 
+<<<<<<< HEAD
         picture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,5 +65,36 @@ public class ProfileFragment extends Fragment {
                         .start(getActivity());
             }
         });
+=======
+        ApiProvider apiProvider = new ApiProvider();
+        apiProvider.getCategories(new ApiListener<ArrayList<Category>>() {
+            @Override
+            public void onSuccess(ArrayList<Category> response) {
+                List<String> categoryList = new ArrayList<>();
+
+                for(Category category: response) {
+                    categoryList.add(category.getName());
+                }
+
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, categoryList);
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner1.setAdapter(adapter1);
+
+                ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, categoryList);
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner2.setAdapter(adapter2);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+                Toast.makeText(getActivity().getApplicationContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_save_profile) public void saveProfileButtonPressed(Button button) {
+        // TODO
+>>>>>>> Hydrate my profile fragment - 2
     }
 }
